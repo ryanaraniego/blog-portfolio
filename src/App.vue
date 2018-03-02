ded80717b94f86b23f606285d39041bde0d56e61
ryanaraniego <ryanaraniego@gmail.com>
2018-03-02 13:13:25 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile >
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile >
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>My Portfolio</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout row wrap>
+          <v-flex
+            xs12
+            sm6
+            md8
+            text-xs-center
+            layout
+            align-center
+            justify-center
+          >
+            <v-avatar
+              :tile="tile"
+              :size="avatarSize"
+              class="grey lighten-4"
+            >
+              <img src="src/assets/logo.PNG">
+            </v-avatar>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    slider: 256,
+    tile: false
+  }),
+  props: {
+    source: String
+  },
+  computed: {
+      avatarSize () {
+        return `${this.slider}px`
+      }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      slider: 56,
+      tile: false
+    }),
+
+    computed: {
+      avatarSize () {
+        return `${this.slider}px`
+      }
+    }
+  }
+</script>
